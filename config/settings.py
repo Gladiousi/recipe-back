5dbd7aadeeab5f061cf2d864fb38527403028c0f
gladiousi <abramovdaniil426475@gmail.com>
2026-02-06 11:01:13 +0300

    fix

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = config('SECRET_KEY', default='your-secret-key-change-in-production'
 
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'recipe-back-production.up.railway.app']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
