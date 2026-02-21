22db929889c9afa868cb565bf63947cbce3ee434
gladiousi <abramovdaniil426475@gmail.com>
2026-02-21 19:01:30 +0300

    fix

--- config/settings.py
+++ config/settings.py
@@ -130,7 +130,7 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://recipe-decolimq0-gladiousis-projects.vercel.app/"
+    "https://recipe-decolimq0-gladiousis-projects.vercel.app"
 ]
 
 if DEBUG:
